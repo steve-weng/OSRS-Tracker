32be44662caca3c0b378d5bed98003be16a2574a
S <51041031+steve-weng@users.noreply.github.com>
2024-04-19 16:26:43 -0400

    access api individual item ID, migration
    migrated to one file and see summ.

--- trackerOSRS.py
+++ trackerOSRS.py
@@ -1,21 +1,45 @@
 import sqlite3
+import requests
+
 
 con = sqlite3.connect("test5.db")
 cur = con.cursor()
 
-cur.execute("CREATE TABLE items(id,name, high_price, low_price)")
+#.execute("CREATE TABLE items(id,name, high_price, low_price)")
 
-res = cur.execute("SELECT name FROM sqlite_master")
-print(res.fetchone())
+#res = cur.execute("SELECT name FROM sqlite_master")
+#print(res.fetchone())
 
 # test item, we'll get this from the front end
-itemID = "13190"
-itemName = "Old school bond"
-high_price = 1000 # and these from scraping the site
-low_price = 999
+# itemID = "13190"
+# itemName = "Old school bond"
+# high_price = 1000 # and these from scraping the site
+# low_price = 999
 
-cur.execute("INSERT INTO items VALUES (?, ?, ?, ?)", 
-	(itemID, itemName, high_price, low_price))
+# cur.execute("INSERT INTO items VALUES (?, ?, ?, ?)", 
+# 	(itemID, itemName, high_price, low_price))
 
-for row in cur.execute("SELECT * FROM items"):
-	print(row)
+# for row in cur.execute("SELECT * FROM items"):
+# 	print(row)
+
+headers = {
+	
+	'User-Agent' : 'learning to make a price alert',
+	'From': 'testing@gmail.com'
+}
+
+response = requests.get('https://prices.runescape.wiki/api/v1/osrs/5m', headers=headers)
+
+if response.status_code == 200:
+	print ("connected")
+	print ("---------------------------------")
+	data = response.json()
+	#print(data)
+elif response.status_code == 404:
+	print ("unable to reach")
+else:
+	print ("failed to connect")
+
+print (type(data))
+print(data["data"]["2"]) # we want the data, and the second identifier is the item ID
+# is a dict, so need to transform this dict into a table
