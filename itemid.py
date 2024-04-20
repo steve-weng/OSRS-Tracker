941bcbf5945103f89eb1b6e1d4ee2e93b3ef6f0b
S <51041031+steve-weng@users.noreply.github.com>
2024-04-20 01:39:23 -0400

    changed all python requests approach to pandas
    pandas can do this directly, 50 lines of code to 10.  Pickling the saved data.

--- itemid.py
+++ itemid.py
@@ -1,42 +1,17 @@
 import pandas as pd
-from bs4 import BeautifulSoup
 
-import requests
+#url = 'https://static.runelite.net/api/runelite-api/constant-values.html'
+#url = 'https://static.runelite.net/api/runelite-api/net/runelite/api/ItemID.html'
+url = 'https://static.runelite.net/api/runelite-api/constant-values.html#net.runelite.api.ItemID'
 
-url = 'https://static.runelite.net/api/runelite-api/constant-values.html'
-r = requests.get(url)
+tables = pd.read_html('https://static.runelite.net/api/runelite-api/constant-values.html')
+df = tables[9] #currently 9th table on this page is the itemID table
 
-if r.status_code == 200:
-	print ("connected")
-	print ("---------------------------------")
-	data = r.text
-	#print(data)
-elif response.status_code == 404:
-	print ("unable to reach")
-else:
-	print ("failed to connect")
+df.to_pickle("ItemIDs.pkl")
 
-soup = BeautifulSoup(data, "html.parser")
-#tables = pd.read_html(str(soup))
-#with open('saved.html', 'w') as f:
-#	f.write(str(soup))
+df_unpickle = pd.read_pickle("ItemIDs.pkl")
 
-#print (soup)
-#soupTR = soup.find_all('tr')
-#with open('saved.html', 'w') as f:
-#	f.write(str(soupTR))
-
-soupTable = soup.find_all('table')
-with open('s1.html', 'w') as f:
-	f.write(str(soupTable[0]))
-
-df = pd.read_html(str(soupTable[0]))
-print(df)
-# # Read the HTML file into a Pandas dataframe
-# with open('https://static.runelite.net/api/runelite-api/constant-values.html') as file:
-#     soup = BeautifulSoup(file, 'html.parser')
-# tables = pd.read_html(str(soup))
-
+print(df_unpickle)
 
 
 
