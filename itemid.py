a070adad4f411ba7746de912ea39fcd5542d47a6
S <51041031+steve-weng@users.noreply.github.com>
2024-04-22 02:16:25 -0400

    code cleanup

--- itemid.py
+++ itemid.py
@@ -1,20 +1,10 @@
 import pandas as pd
 
-#url = 'https://static.runelite.net/api/runelite-api/constant-values.html'
-#url = 'https://static.runelite.net/api/runelite-api/net/runelite/api/ItemID.html'
-url = 'https://static.runelite.net/api/runelite-api/constant-values.html#net.runelite.api.ItemID'
-
 tables = pd.read_html('https://static.runelite.net/api/runelite-api/constant-values.html')
 df = tables[9] #currently 9th table on this page is the itemID table
 
 df.to_pickle("ItemIDs.pkl")
 
+# double check the pickled file and table saved
 df_unpickle = pd.read_pickle("ItemIDs.pkl")
-
 print(df_unpickle)
-
-
-
-
-
-
