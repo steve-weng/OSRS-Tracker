2aa0f72f6dff1bc5f27fe19fd81e817c4c729b81
S <51041031+steve-weng@users.noreply.github.com>
2024-04-22 13:13:48 -0400

    cleanup
    del unused files

--- item.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from bs4 import BeautifulSoup
-
-import requests
-
-url = 'https://static.runelite.net/api/runelite-api/constant-values.html'
-
-dfs = pd.read_html(url)
-display(dfs)
